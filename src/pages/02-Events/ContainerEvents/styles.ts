7c1d05a6580845294f7a1f3379993039214e496b
Leorrc <leorrc1@gmail.com>
2022-09-12 16:45:49 -0300

    add banner page events

--- src/pages/02-Events/ContainerEvents/styles.ts
+++ src/pages/02-Events/ContainerEvents/styles.ts
@@ -1,11 +1,20 @@
 import styled from "styled-components";
+import banner from '../../../images/full/party.svg'
+import pixelToRem from "../../../utils/pxToRem";
 
 export const Section = styled.section`
-  padding: calc(5rem + var(--header-height)) 0;
+  padding: calc(12rem + var(--header-height)) 0;
 
-  @media (min-width: 992px) {
-    padding: calc(10rem + var(--header-height)) 0;
-    background: var(--body-color);
+  background-image: url(${banner});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center ;
+  width: 100%;
+  height: 100%;
 
+  @media (min-width: 1440px) {
+    padding: calc(12rem + var(--header-height)) 0;
+    margin-top: ${pixelToRem(72)}
   }
 `;
+
