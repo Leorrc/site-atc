16514a7752c9d9a4bf19c55d35c01d7c87cd9c6b
Leonardo Carvalho <leorrc1@gmail.com>
2023-04-12 16:46:44 -0300

    fix "*" route

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { Parceiros } from './pages/04-Parceiros/parceiros'
 import { Footer } from './components/03-Footer'
 import { Affiliates } from './pages/03-Affiliates/affiliates'
 import { Leitura } from './pages/05-Leitura/leitura'
+import { Noticias } from './pages/06-Noticias/noticias'
 import ScrollToTop from './components/01-Utils/ScrollTop'
 import ScrollToTopButton from './components/01-Utils/ScrollTopButton'
 
@@ -19,11 +20,13 @@ function App() {
       <Header />
 
       <Routes>
+        <Route path="*" element={<Home />} />
         <Route path="/" element={<Home />} />
         <Route path="/events" element={<Events />} />
         <Route path="/affiliates" element={<Affiliates />} />
         <Route path="/parceiros" element={<Parceiros />} />
         <Route path="/leitura" element={<Leitura />} />
+        <Route path="/noticias" element={<Noticias />} />
       </Routes>
       <ScrollToTopButton />
       <Footer />
