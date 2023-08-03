06cef8c79a71e163314839791a0b7fc4cbac2d2b
Leonardo Carvalho <100796907+Leorrc@users.noreply.github.com>
2023-08-03 09:02:57 -0300

    Update index.tsx

--- src/components/02-Header/SideBar/index.tsx
+++ src/components/02-Header/SideBar/index.tsx
@@ -47,7 +47,7 @@ export function SideBar({ isOpen, toggle }: Props) {
             </SidebarRoute>
             <SidebarScroll>
               <a
-                href="https://docs.google.com/forms/d/e/1FAIpQLScKF3mjS4fIwIo2CEBjssFwoNfBSRY647jYoF-xoAg5FK3n0w/viewform"
+                href="https://forms.gle/fw8AfpVkhNFcdTRA6"
                 target="_blank"
                 rel="noreferrer"
               >
